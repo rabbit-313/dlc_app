e7f04d052165172cf73af088c7e5a0448ff9fccb
rabbit0624 <migiso1051@i.softbank.jp>
2023-05-06 18:54:11 +0900

    ru

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -6,8 +6,7 @@ app_name='MainApp'
 urlpatterns=[
     # path('',views.IndexView.as_view(), name="index"),
     path('',views.index, name="index"),
-    
     path('introduce/',views.IndexView.as_view(), name="introduce"),
     path('propose/',views.IndexView.as_view(), name="propose"),
-    path('membership/',views.IndexView.as_view(), name="membership"),
+    path('membership/',views.membership, name="membership"),
 ]
