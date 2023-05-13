69ba6aa5323e1224a7dae4943ad89891fc58c90c
Takaki-S-1 <copupon1213@icloud.com>
2023-05-13 11:00:58 +0900

    change

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,21 +1,42 @@
-from django.shortcuts import render
-
-
-# Create your views here.
-def index(request):
-   context = {'sample':'HelloWorld'}
-   return render(request, 'index.html', context)
-
 from django.views import generic
 
-class IndexView(generic.TemplateView):
-    template_name="introduce.html"
-    
-class IndexView(generic.TemplateView):
-    template_name="introduce.html"
-    
-class IndexView(generic.TemplateView):
-    template_name="propose.html"
-class IndexView(generic.TemplateView):
-    template_name="menbership.html"
+import csv
+from django.views import View
+from django.shortcuts import render
+from django.views.generic import TemplateView
 
+class IndexView(generic.TemplateView):
+    template_name="index.html"
+    
+class IntroduceView(generic.TemplateView):
+    template_name="introduce.html"
+    
+class ProposingView(generic.TemplateView):
+    template_name="proposing.html"
+class MembershipView(generic.TemplateView):
+    template_name="membership.html"
+
+
+class MembershipView(TemplateView):
+    template_name = "membership.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            # CSVファイルがアップロードされたときの処理
+            csv_file = request.FILES['csv_file']
+            data = {}
+            reader = csv.DictReader(csv_file.read().decode('utf-8').splitlines())
+            for row in reader:
+                genre = row['ジャンル']
+                if genre not in data:
+                    data[genre] = []
+                data[genre].append({'age': row['年齢'], 'name': row['名前']})
+            for genre in data:
+                data[genre] = sorted(data[genre], key=lambda x: x['age'])
+            sorted_data = sorted(data.items(), key=lambda x: x[0])
+            return render(request, self.template_name, {'sorted_data': sorted_data})
+        else:
+            return render(request, self.template_name)
